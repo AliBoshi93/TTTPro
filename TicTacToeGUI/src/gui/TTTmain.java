bb3179e3d04611396e1c16b605cd4445e06f80be
ABOS137 <ABOS137@PC02180108.cc-college.gecs.loc>
2019-11-25 08:18:49 +0100

    1

--- TicTacToeGUI/src/gui/TTTmain.java
+++ TicTacToeGUI/src/gui/TTTmain.java
@@ -2,7 +2,7 @@ package gui;
 
 import javax.swing.SwingUtilities;
 
-public class main {
+public class TTTmain {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
